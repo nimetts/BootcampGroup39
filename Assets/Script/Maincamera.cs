9c889790409d398863e9449f9ef1555f578b8b7b
MuhammedHadiAlsafi <m.hadi.alsafi@gmail.com>
2024-08-01 21:18:00 +0300

    düzeltmeler

---
+++ Assets/Script/Maincamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maincamera : MonoBehaviour
+{
+    private GameObject tr;
+    private GameObject camera;
+
+    void Start()
+    {
+        tr = GameObject.FindWithTag("Player");
+    }
+
+    void Update() {
+        transform.position = new Vector3 (tr.transform.position.x , tr.transform.position.y, -10);
+    }
+}
